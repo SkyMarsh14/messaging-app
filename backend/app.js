def4fb90983467908fd1f951b6829eb7be368176
Sky Randy <soras1758@gmail.com>
2025-11-16 09:32:37 +1030

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express app started - listening on port ${PORT}`);
+});
