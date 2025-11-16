89467eda920a2f326a4812a0503634a7607bebda
Sky Randy <soras1758@gmail.com>
2025-11-17 07:53:18 +1030

    add prisma migration file, and passport config file

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import session from "express-session";
+import passport from "passport";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ backend/config/passport.js
@@ -0,0 +1,40 @@
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import prisma from "../db/prisma.js";
+
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username,
+        },
+      });
+      if (!user) return done(null, false, { message: "User not found." });
+      const match = password === user.password;
+      if (!match)
+        return done(null, false, {
+          message: "Either password or username is wrong.",
+        });
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
