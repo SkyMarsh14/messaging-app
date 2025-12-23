30ed50ebc0f063fdfa9f9dcc772f6aca44bacad4
Sky Randy <soras1758@gmail.com>
2025-12-24 05:03:02 +1030

    change router names

--- frontend/src/api/EndPoints.js
+++ frontend/src/api/EndPoints.js
@@ -2,7 +2,7 @@ const ENDPOINTS = {
   login: () => import.meta.env.VITE_BACKEND_URL + "auth/login",
   signup: () => import.meta.env.VITE_BACKEND_URL + "auth/create",
   users: () => import.meta.env.VITE_BACKEND_URL + "user/others",
-  messages: (chatRoomId) =>
-    import.meta.env.VITE_BACKEND_URL + `message/chat/${chatRoomId}`,
+  messages: (userId) =>
+    import.meta.env.VITE_BACKEND_URL + `message/user/${userId}`,
 };
 export default ENDPOINTS;
--- frontend/src/components/ChatPane.jsx
+++ frontend/src/components/ChatPane.jsx
@@ -2,16 +2,19 @@ import styled from "styled-components";
 import ENDPOINTS from "../api/EndPoints";
 import { useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import Error from "../page/Error";
 const Wrapper = styled.div`
   grid-column: 2/3;
 `;
 const Messages = styled.div``;
 
 const ChatPane = () => {
-  const { chatRoomId } = useParams();
-  const url = ENDPOINTS.messages(chatRoomId);
+  const { userId } = useParams();
+  const url = ENDPOINTS.messages(userId);
   const { data, error, loading, needsAuth } = useFetch(url);
-
+  if (error) {
+    throw new Error("Requested route does not exist");
+  }
   return (
     <Wrapper>
       <Messages></Messages>
