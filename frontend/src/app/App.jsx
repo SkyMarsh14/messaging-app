8d14ac1c8e5c47f126d3d4d326c685badaceb34d
Sky Randy <soras1758@gmail.com>
2026-01-10 09:57:15 +1030

    add light theme, read and saves theme preferences from browser

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,12 +2,24 @@ import UserContext from "../helper/UserContext.js";
 import routes from "./routes.jsx";
 import GlobalStyle from "./globalStyle.jsx";
 import { ThemeProvider } from "styled-components";
-import theme from "../assets/darkTheme.js";
+import darkTheme from "../assets/darkTheme.js";
+import lightTheme from "../assets/lightTheme.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useState } from "react";
 
 const router = createBrowserRouter(routes);
 const App = () => {
+  const prefersDarkTheme = () => {
+    const savedThemePreference = localStorage.getItem("darkMode");
+    if (savedThemePreference === null) {
+      localStorage.setItem("darkMode", "true");
+      return window.matchMedia("(prefers-color-scheme: dark)").matches; // Reads browser theme preferenece
+    }
+    return savedThemePreference == "true";
+  };
+  const [currentTheme, setCurrentTheme] = useState(
+    prefersDarkTheme() ? darkTheme : lightTheme
+  );
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   const [roomData, setRoomData] = useState(null);
   const [chatData, setChatData] = useState(null);
@@ -23,12 +35,13 @@ const App = () => {
         setChatData,
         selectedRoom,
         setSelectedRoom,
+        setCurrentTheme,
       }}
     >
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={currentTheme}>
         <GlobalStyle
-          bodyBgColor={theme.primaryBgColor}
-          textColor={theme.primaryTextColor}
+          bodyBgColor={currentTheme.primaryBgColor}
+          textColor={currentTheme.primaryTextColor}
         />
         <RouterProvider router={router} />
       </ThemeProvider>
